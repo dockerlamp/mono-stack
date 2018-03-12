b048f426fc448777bdd2ae8721279c5d42ad2476
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-03-13 00:44:03 +0100

    test(bus): command and write model test

--- src/model/bus.test.ts
+++ src/model/bus.test.ts
@@ -1,47 +1,41 @@
+import {} from 'jest';
+import * as uuid from 'uuid';
+
 import { CommandBus } from './command-bus/CommandBus';
 import { LoginUserHandler } from './command-handler/LoginUserHandler';
 import { LoginUserCommand } from './command/LoginUser';
 import { UserWriteModel } from './write-model/UserWriteModel';
-import * as uuid from 'uuid';
-import { UserReadModel } from './read-model/UserReadModel';
 import { ILoginUser } from './command/ILoginUser';
 
-async function test() {
-    // configure command bus
-    let bus = new CommandBus();
-    let writeModel = new UserWriteModel();
-    let readModel = new UserReadModel(writeModel);
-    let handler = new LoginUserHandler(writeModel);
-    bus.registerCommandHandler(handler);
+describe('CQRS', () => {
+    let bus: CommandBus;
+    let writeModel: UserWriteModel;
 
-    let sessionId = '12345-123-123';
-    let command: ILoginUser = {
-        name: 'login-user',
-        id: uuid.v4(),
-        user: {
-            email: 'foo@bar.com',
-            userName: 'John Doe',
-            sessionId: '12345-123-123'
-        }
-    };
+    beforeEach(() => {
+        // configure command bus
+        bus = new CommandBus();
+        writeModel = new UserWriteModel();
+        let handler = new LoginUserHandler(writeModel);
+        bus.registerCommandHandler(handler);
+    });
 
-    await bus.sendCommand(command);
-    let command2 = {
-        name: 'login-user',
-        id: uuid.v4(),
-        user: {
-            email: 'foo@bar.com',
-            userName: 'John Lennon',
-            sessionId: '54321-123-123'
-        }
-    };
+    it('sending loginUser command should call saveUser on write model', async () => {
+        let command: ILoginUser = {
+            name: 'login-user',
+            id: uuid.v4(),
+            user: {
+                email: 'foo@bar.com',
+                userName: 'John Doe',
+                sessionId: uuid.v4()
+            }
+        };
 
-    await bus.sendCommand(command2);
-    // wait until user returned
-    let user = await readModel.getUserBySessionId(command.user.sessionId);
-    console.log(`User from read model by session Id=${command.user.sessionId}`, user);
-}
+        let user;
+        jest.spyOn(writeModel, 'saveUser').mockImplementation((userData) => {
+            user = userData;
+        });
 
-test()
-    .then(() => console.log('done'))
-    .catch((err) => console.error(err));
+        await bus.sendCommand(command);
+        expect(user).toEqual(command.user);
+    });
+});
