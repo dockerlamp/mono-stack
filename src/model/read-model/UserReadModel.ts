e2ecef44e0f66e4783ea9c2b6e9e5d21b49bb6e2
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-03-13 00:43:08 +0100

    feat(read-model): user read model emits event when read model changes

--- src/model/read-model/UserReadModel.ts
+++ src/model/read-model/UserReadModel.ts
@@ -3,14 +3,19 @@ import { EventEmitter } from 'events';
 /**
  * Create read model using write model events
  */
-export class UserReadModel {
+export class UserReadModel extends EventEmitter {
     private sessions: {[sessioId: string]: any} = {};
 
     constructor(private userEmitter: EventEmitter) {
+        super();
         let updateSessions = (user) => {
             // index users by sessionId
             user.sessionIds.forEach((sessionId) => {
+                let prevValue = this.sessions[sessionId];
                 this.sessions[sessionId] = user;
+                if (prevValue !== user) {
+                    this.emit('session-index-updated', sessionId);
+                }
             });
         };
         this.userEmitter.on('updated', updateSessions);
