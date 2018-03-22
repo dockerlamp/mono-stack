c820bbcc8015d35860529c4ad83528fef84e2d5c
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-03-23 00:27:53 +0100

    feat(front): store session in redis

--- src/front/ApplicationFactory.ts
+++ src/front/ApplicationFactory.ts
@@ -2,25 +2,45 @@ import { Express } from 'express';
 import * as express from 'express';
 import * as session from 'express-session';
 import * as morgan from 'morgan';
+import * as connectRedis from 'connect-redis';
+import { RedisStoreOptions } from 'connect-redis';
+import * as Redis from 'ioredis';
 
 import { AuthController } from './controllers/AuthController';
 import { IController } from './controllers/IController';
 import { RootController } from './controllers/RootController';
+import { config } from './config';
+
+/* tslint:disable-next-line:variable-name */
+const RedisStore = connectRedis(session);
 
 export class ApplicationFactory {
     public async createApplication(): Promise<Express> {
         let app = express();
         app.use(morgan('tiny'));
-        app.use(session({
-            secret: 'mono-stack',
-            resave: false,
-            saveUninitialized: true,
-            unset: 'destroy',
-        }));
+        app.use(this.getSessionMiddleware());
 
         await this.setupControllers(app);
 
         return app;
+    }
+
+    private getSessionMiddleware(): express.RequestHandler {
+        let redisClient: any = new Redis({
+            port: config.session.redis.port,
+            host: config.session.redis.host
+        });
+        let redisConfig: RedisStoreOptions = {
+            client: redisClient
+        };
+
+        return session({
+            secret: 'mono-stack',
+            resave: false,
+            saveUninitialized: true,
+            unset: 'destroy',
+            store: new RedisStore({client: redisClient}),
+        });
     }
 
     private getControllers(): IController[] {
--- src/front/config.ts
+++ src/front/config.ts
@@ -3,17 +3,29 @@ import rc = require('rc');
 // @TODO definicja domyslnych wartosci, mozliwych kluczy
 export interface IConfig {
     port: number;
+    session: {
+        redis: {
+            port: number;
+            host: string;
+        };
+    };
     oAuthApps: {
         gitHub: {
-            clientID: string,
-            clientSecret: string
-            callbackURL: string
-        }
+            clientID: string;
+            clientSecret: string;
+            callbackURL: string;
+        };
     };
 }
 
 let defaultConfig = {
-    port: 3000
+    port: 3000,
+    session: {
+        redis: {
+            port: 6379,
+            host: 'redis-session',
+        }
+    }
 };
 
 export let config: IConfig = rc('monostack', defaultConfig);
