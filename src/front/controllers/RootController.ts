ad088040717cb6dd092890779c82227659cdc805
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-03-30 03:22:33 +0200

    fix(*): fix async handlers and tests

--- src/front/controllers/RootController.ts
+++ src/front/controllers/RootController.ts
@@ -6,39 +6,47 @@ import { GetSessionUser } from '../../model/query/GetSessionUser';
 
 export class RootController implements IController {
     public async initRoutings(app: Express): Promise<void> {
-        app.get('/', async (req, res) => {
-            // Query:
-            //    v1 let user = await query.waitGithubUser(req.session.passport.user.id);
-            //    v2 let user = await query.waitForGitHubUser(commandId);
-            //    v3 let user = await query.waitForGitHubUser(session.id); //ok
-
-            if (req.session.loginInProgress) {
-                let getSessionUser = new GetSessionUser(userReadModel);
-                let user = await getSessionUser.query(req.session.id);
-                if (user) {
-                    req.session.user = user;
-                    req.session.loginInProgress = undefined;
-                }
-            }
-
-            if (req.session.user) {
-                res.send(
-                    `Hi, you are logged as ${req.session.user.name}, ` +
-                    `<a href="/logout">logout</a>`
-                );
-            } else {
-                let displayName = req.session.loginInProgress ? 'login in progress' : 'you are anonymous';
-                res.send(`Hi, ${displayName}, <a href="/login/github">sign in</a> with github account`);
-            }
+        app.get('/', async (req, res, next) => {
+            this.rootHandler(req, res).catch(next);
         });
 
-        app.get('/logout',  (req, res) => {
-            req.session = null;
-            res.redirect('/');
+        app.get('/logout', async (req, res, next) => {
+            this.logoutHandler(req, res).catch(next);
         });
     }
 
     public async initMiddlewares(app: Express): Promise<void> {
         // no middlewares
+    }
+
+    private async rootHandler(req, res) {
+        // Query:
+        //    v1 let user = await query.waitGithubUser(req.session.passport.user.id);
+        //    v2 let user = await query.waitForGitHubUser(commandId);
+        //    v3 let user = await query.waitForGitHubUser(session.id); //ok
+
+        if (req.session.loginInProgress) {
+            let getSessionUser = new GetSessionUser(userReadModel);
+            let user = await getSessionUser.query(req.session.id);
+            if (user) {
+                req.session.user = user;
+                req.session.loginInProgress = undefined;
+            }
+        }
+
+        if (req.session.user) {
+            res.send(
+                `Hi, you are logged as ${req.session.user.name}, ` +
+                `<a href="/logout">logout</a>`
+            );
+        } else {
+            let displayName = req.session.loginInProgress ? 'login in progress' : 'you are anonymous';
+            res.send(`Hi, ${displayName}, <a href="/login/github">sign in</a> with github account`);
+        }
+    }
+
+    private async logoutHandler(req, res) {
+        req.session = null;
+        res.redirect('/');
     }
 }
