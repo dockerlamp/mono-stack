986e1ada777dbc6f0974e0352550bf38e9da3e58
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-03-31 02:30:41 +0200

    feat(front): add session validator and error controller

---
+++ src/front/controllers/ErrorController.ts
@@ -0,0 +1,25 @@
+import { Express } from 'express';
+
+import { IController } from './IController';
+import { userReadModel } from '../../model/command-bus';
+import { GetSessionUser } from '../../model/query/GetSessionUser';
+
+export class ErrorController implements IController {
+    public async initRoutings(app: Express): Promise<void> {
+        app.use((err, req, res, next) => {
+            console.error(err);
+            if (res.headersSent) {
+                return next(err);
+            }
+            let errorResponse = {
+                error: 'Unhandlerd error',
+                message: err.message,
+            };
+            res.status(500).send(errorResponse);
+        });
+    }
+
+    public async initMiddlewares(app: Express): Promise<void> {
+        // no middlewares
+    }
+}
