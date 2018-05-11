52529da1d42afdca970a499c96d7c0691a6efd11
Grzegorz Marchwinski <yankeepl@gmail.com>
2018-05-11 02:33:49 +0200

    refactor(user-service): extract findLoginUser method

--- src/model/user/service/UserService.ts
+++ src/model/user/service/UserService.ts
@@ -3,13 +3,24 @@ import * as _ from 'lodash';
 
 import { UserModel } from '../model/UserModel';
 import { ILoginUser } from './ILoginUser';
-import { IUserDocument } from '../model/types';
+import { IUserDocument, IUser } from '../model/types';
 
 @Service()
 export class UserService {
     constructor(private userModel: UserModel) { }
 
-    public async login(loginUser: ILoginUser) {
+    public async login(loginUser: ILoginUser): Promise<IUser> {
+        let user = await this.findLoginUser(loginUser);
+        if (user) {
+            // if exist then update only fields which does not exists in user
+            return await this.updateUserUndefinedFields(user, loginUser);
+        } else {
+            // if doesnt exists add new user
+            return await this.userModel.insertUser(loginUser);
+        }
+    }
+
+    private async findLoginUser(loginUser: ILoginUser): Promise<IUserDocument> {
         let {provider, providerUserId, email} = loginUser;
         // get user by provider id
         let user = await this.userModel.getUserByProvider(provider, providerUserId);
@@ -17,16 +28,11 @@ export class UserService {
             // get user by email
             user = await this.userModel.getUseByEmail(email);
         }
-        if (user) {
-            // if exist then update only fields which does not exists in user
-            await this.updateUserUndefinedFields(user, loginUser);
-        } else {
-            // if doesnt exists add new user
-            await this.userModel.insertUser(loginUser);
-        }
+
+        return user;
     }
 
-    private async updateUserUndefinedFields(user: IUserDocument, loginPayload: ILoginUser) {
+    private async updateUserUndefinedFields(user: IUserDocument, loginPayload: ILoginUser): Promise<IUserDocument> {
         let updateData = {};
         let compareFields = {
             firstName: loginPayload.firstName,
@@ -45,5 +51,7 @@ export class UserService {
         if (user.isModified) {
             await user.save();
         }
+
+        return user;
     }
 }
