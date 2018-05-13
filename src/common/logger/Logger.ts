a49d1f80352d6cb67eb3d050abd1ee08dbef51cc
Pawel Maciejski <pawel.maciejski@gmail.com>
2018-05-13 18:01:20 +0200

    feat(*): add logger service

--- src/common/logger/Logger.ts
+++ src/common/logger/Logger.ts
@@ -1,26 +1,25 @@
-const consoleLogFunctionNames = [
-    'log',
-    'info',
-    'warn',
-    'error',
-    'debug',
-];
+import { Service } from 'typedi';
+import * as winston from 'winston';
+import { config } from './config/default';
 
-let defaultLoggerFunctions = {};
-consoleLogFunctionNames.forEach((logFunctionName) => {
-    consoleLogFunctionNames[logFunctionName] = console[logFunctionName];
-});
-
+@Service()
 export class Logger {
-    public static disableConsoleLogger() {
-        consoleLogFunctionNames.forEach((logFunctionName) => {
-            console[logFunctionName] = () => {
-                // disable log
-            };
+    public getLogger(): winston.Logger {
+        let transport;
+        let option = config.useOption;
+        switch (option) {
+            case 'console':
+                transport = new winston.transports.Console(config.options[option]);
+                break;
+            case 'file':
+                transport = new winston.transports.File(config.options[option]);
+                break;
+            default:
+                throw new Error(`Unknown transport medium for logging ${option}`);
+        }
+        let logger = new winston.Logger({
+            transports : [transport],
         });
+        return logger;
     }
-}
-
-if (process.env.DISABLE_CONSOLE_LOGGER) {
-    Logger.disableConsoleLogger();
 }
