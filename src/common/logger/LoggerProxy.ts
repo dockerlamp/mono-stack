a0e1010cc01d28985d70da6c4568bf5e670d5a19
Pawel Maciejski <pawel.maciejski@gmail.com>
2018-05-19 18:21:35 +0200

    refactor(logs): LoggerProxy close to SOLID principles

--- src/common/logger/LoggerProxy.ts
+++ src/common/logger/LoggerProxy.ts
@@ -1,4 +1,5 @@
 import { Service } from 'typedi';
+import * as _ from 'lodash';
 import * as winston from 'winston';
 
 import { FrontConfigProvider } from '../../front/config/FrontConfigProvider';
@@ -10,33 +11,37 @@ export class LoggerProxy {
     constructor(private frontConfigProvider: FrontConfigProvider) {}
 
     public getLogger(): winston.Logger {
-        if (this.loggerInstance) {
-            return this.loggerInstance;
+        if (!this.loggerInstance) {
+            this.loggerInstance = this.createLoggerInstance();
         }
+        return this.loggerInstance;
+    }
 
+    private createLoggerInstance() {
+        return new winston.Logger({
+            transports : this.getTransports(),
+        });
+    }
+
+    private getTransports(): any {
         let loggerConfig = this.frontConfigProvider.getConfig().logger;
         let enabledTransports = [];
 
+        // @TODO refactor using chain & map from _
         for (let name in loggerConfig.transports) {
             if (!loggerConfig.transports[name].enabled) { continue; }
-            let transport;
-            switch (name) {
-                case 'console':
-                    transport = new winston.transports.Console(loggerConfig.transports[name].config);
-                    break;
-                case 'file':
-                    transport = new winston.transports.File(loggerConfig.transports[name].config);
-                    break;
-                default:
-                    throw new Error(`Unknown transport medium for logging: ${name}`);
-            }
-            enabledTransports.push(transport);
+            let transportInstance = this.getTransportInstance(name, loggerConfig.transports[name].config);
+            enabledTransports.push(transportInstance);
         }
+        return enabledTransports;
+    }
 
-        this.loggerInstance = new winston.Logger({
-            transports : enabledTransports,
-        });
-
-        return this.loggerInstance;
+    private getTransportInstance(transportName, transportConfigItem): any {
+        let transportTypeMap = {
+            console : winston.transports.Console,
+            file: winston.transports.File,
+        };
+        let transportType = transportTypeMap[transportName];
+        return new transportType(transportConfigItem);
     }
 }
--- test/integration/LoggerProxy.test.ts
+++ test/integration/LoggerProxy.test.ts
@@ -3,11 +3,12 @@ import * as _ from 'lodash';
 
 import { FrontConfigProvider } from '../../src/front/config/FrontConfigProvider';
 import { getTestLogger } from './helpers/getTestLogger';
+import { IFrontConfig } from '../front/config/IFrontConfig';
 
 export const TEST_LOG_FILENAME = 'test.log';
 
 describe('LoggerProxy', () => {
-    let testConfig;
+    let testConfig: IFrontConfig;
 
     beforeEach(async () => {
         let configProvider = Container.get(FrontConfigProvider);
@@ -21,8 +22,8 @@ describe('LoggerProxy', () => {
         testConfig.logger.transports.file.config.filename = TEST_LOG_FILENAME;
         let logger = getTestLogger(testConfig);
 
-        expect('file' in logger.transports).toBeTruthy();
-        expect('console' in logger.transports).toBeFalsy();
+        expect(logger.transports.file).toBeDefined();
+        expect(logger.transports.console).toBeUndefined();
     });
 
     it('should return logger with only console transport set', async () => {
@@ -30,8 +31,8 @@ describe('LoggerProxy', () => {
         testConfig.logger.transports.console.enabled = true;
         let logger = getTestLogger(testConfig);
 
-        expect('file' in logger.transports).toBeFalsy();
-        expect('console' in logger.transports).toBeTruthy();
+        expect(logger.transports.file).toBeUndefined();
+        expect(logger.transports.console).toBeDefined();
     });
 
     it('should return logger without transport set', async () => {
@@ -39,7 +40,7 @@ describe('LoggerProxy', () => {
         testConfig.logger.transports.console.enabled = false;
         let logger = getTestLogger(testConfig);
 
-        expect('file' in logger.transports).toBeFalsy();
-        expect('console' in logger.transports).toBeFalsy();
+        expect(logger.transports.file).toBeUndefined();
+        expect(logger.transports.console).toBeUndefined();
     });
 });
